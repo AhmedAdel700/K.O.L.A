a9d0456bed13878c745b486249ca94568a224fd4
Ahmed Adel <AhmedAdel.Engineer1@gmail.com>
2026-01-22 13:42:34 +0200

    added silde type 2 but not compelte

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ export default function Header({ type = "popup" }: HeaderProps) {
   ];
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-transparent">
+    <header className="fixed top-0 left-0 right-0 z-100 bg-transparent">
       <div className="container mx-auto px-4 h-16 flex items-center justify-between relative z-50">
         {/* Logo */}
         <TransitionLink href="/" className="flex items-center gap-2 font-bold text-xl hover:opacity-80 transition-opacity">
--- src/components/Slider/MainSlider.tsx
+++ src/components/Slider/MainSlider.tsx
@@ -1,4 +1,5 @@
 import SliderType1 from "./SliderType1";
+import SliderType2 from "./SilderType2";
 interface MainSliderProps {
   type: "type1" | "type2";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -9,7 +10,7 @@ export default function MainSlider({ type, data }: MainSliderProps) {
     case "type1":
       return <SliderType1 data={data} />;
     case "type2":
-      return null;
+      return <SliderType2 data={data} />;
     default:
       return <SliderType1 data={data} />;
   }
